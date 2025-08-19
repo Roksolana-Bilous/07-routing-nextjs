db421ab5b430fa4de2b55ecc6c8efbcbe60416a9
Roksolana <adelaw283@gmail.com>
2025-08-19 22:21:10 +0300

    layout

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -1,21 +1,19 @@
-import React from "react";
-import css from "./LayoutNotes.module.css";
+  import React from "react";
+import css from "./LayoutNotes.module.css"
 import SidebarNotes from "./@sidebar/default";
 
-export default function NotesLayout(props: {
-  children: React.ReactNode;
-  modal: React.ReactNode;
-}) {
-  return (
-    <section className={css.container}>
-      <aside className={css.sidebar}>
-        <SidebarNotes />
-      </aside>
-      <div className={css.notesWrapper}>
-        {props.children}
-        {props.modal}
-      </div>
-    </section>
-  );
+type NotesClientProps = {
+    children: React.ReactNode;
+    sidebar: React.ReactNode;
 }
+
+const NotesLayout = ({ children, sidebar }: NotesClientProps) => {
+        return (
+        <section className={css.container}>
+            <aside className={css.sidebar}><SidebarNotes /></aside>
+            <div className={css.notesWrapper}>{children}{sidebar}</div>
+        </section>
+    );
+}
+export default NotesLayout 
 
